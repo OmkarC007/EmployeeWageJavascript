72513b76050d3fcb4debbc17051ca66107ea5da9
Om Chikane <chikaneom007@gmail.com>
2022-03-29 18:31:47 +0530

    calculating employee wage using fumction

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -5,18 +5,22 @@ const PART_TIME_HOUR = 4;
 const FULL_TIME_HOURS = 8
 const WAGE_PER_HOUR = 20;
 
-empHours = 0;
 
-let empCheck = Math.floor(Math.random() * 10) % 2 ;
-switch(empCheck){
-    case  IS_PART_TIME :
-        empHours = PART_TIME_HOUR;
-        break;
-    case IS_FULL_TIME:
-        empHours = FULL_TIME_HOURS;
-        break;
-    default:
-        empHours = 0;    
+function getWorkingHours(empCheck){
+    switch(empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOUR;
+            break;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+            break;
+        default:
+            return 0;        
+    }
 }
-let empWage = empHours * WAGE_PER_HOUR
-console.log("emp wage "+empWage);
+
+let empHours ;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+empHours = getWorkingHours(empCheck);
+let empWage = empHours * WAGE_PER_HOUR;
+console.log("Emp Wage "+ empWage); 
