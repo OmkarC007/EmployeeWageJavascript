1187abc29be765ff4149e12ff4e00ea2067db6b0
Om Chikane <chikaneom007@gmail.com>
2022-03-31 08:56:41 +0530

    store daily wage in array

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -19,15 +19,22 @@ function getWorkingHours(empCheck){
             return 0;        
     }
 }
+let empHours = 0;
+function calcDailyWage(empHours){
+    return empHours * WAGE_PER_HOUR;
+}
 
 let totalEmpHrs = 0;
 let totalWorkDays = 0;
+let empWageArr = new Array();
 
 while(totalEmpHrs <= MAX_WORK_HRS_IN_MONTH && totalWorkDays < NUM_OF_WORKING_DAYS){
     totalWorkDays ++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    totalEmpHrs +=getWorkingHours(empCheck);
+    empHours = getWorkingHours(empCheck);
+    totalEmpHrs += empHours;
+    empWageArr.push(calcDailyWage(empHours));
 }
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
-console.log("Total hours " +totalEmpHrs+ " Emp wage "+empWage);
- 
+let empWage = calcDailyWage(totalEmpHrs);
+console.log("Total total Work Days: "+totalWorkDays+" Total hours " +totalEmpHrs+ " Emp wage "+empWage);
+  
